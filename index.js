f899e0dba99f28a32ffa9d8cc043ce55b867bd30
Leandro Ciric <lecirics@gmail.com>
2021-10-22 21:27:25 -0300

    feat: added more connections

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+let users = []
+
 io.on('connection', (socket) => {
+  users.push(socket.id)
+  console.log(users)
+
   socket.on('shot-reachs-left', (msg) => {
-    socket.broadcast.emit('shot-reachs-left', msg);
+    const shotIndex = users.indexOf(socket.id)-1 === -1 ? users.length -1 : users.indexOf(socket.id)-1
+    io.to(users[shotIndex]).emit('shot-reachs-left', msg)
+
+
   });
   socket.on('shot-reachs-right', (msg) => {
-    socket.broadcast.emit('shot-reachs-right', msg);
+    const shotIndex = users.indexOf(socket.id)+1 === users.length ? 0 : users.indexOf(socket.id)+1
+    io.to(users[shotIndex]).emit('shot-reachs-left', msg)
   });
 });
 
