a25dda681bb6cdba8416109b946e1b5eeac975b4
Leandro Ciric <lecirics@gmail.com>
2021-10-22 22:16:51 -0300

    fix: fixed error on refresh page

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ io.on('connection', (socket) => {
   console.log(users)
 
   socket.on('shot-reachs-left', (msg) => {
-    const shotIndex = users.indexOf(socket.id)-1 === -1 ? users.length -1 : users.indexOf(socket.id)-1
+    const shotIndex = users.indexOf(socket.id) - 1 === -1 ? users.length - 1 : users.indexOf(socket.id) - 1
+    console.log('Estou enviando para a direita do ', users[shotIndex])
+
     io.to(users[shotIndex]).emit('shot-reachs-left', msg)
-
-
   });
   socket.on('shot-reachs-right', (msg) => {
-    const shotIndex = users.indexOf(socket.id)+1 === users.length ? 0 : users.indexOf(socket.id)+1
-    io.to(users[shotIndex]).emit('shot-reachs-left', msg)
+    const shotIndex = users.indexOf(socket.id) + 1 === users.length ? 0 : users.indexOf(socket.id) + 1
+    console.log(shotIndex)
+    console.log('Estou enviando para a esquerda do ', users[shotIndex])
+    io.to(users[shotIndex]).emit('shot-reachs-right', msg)
   });
+  socket.on('disconnect', user => {
+    const remove = users.indexOf(socket.id)
+    users.splice(remove, 1)
+  })
 });
 
 server.listen(3000, () => {
